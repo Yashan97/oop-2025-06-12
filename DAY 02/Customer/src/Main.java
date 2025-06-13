4fbe8805c8e4bd1b8b505591deb2cbacbe7f737b
Yashan97 <s.d.y.anupma@gmail.com>
2025-06-13 09:44:27 +0530

    create JFrame

---
+++ DAY 02/Customer/src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame f1 = new JFrame();
+        f1.setVisible(true);
+        f1.setSize(600, 600);
+        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f1.setLocationRelativeTo(null);
+        f1.setVisible(true);
+        f1.setTitle("calculator");
+
+
+
+    }
+}
