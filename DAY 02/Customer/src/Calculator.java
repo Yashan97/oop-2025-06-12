92c170f22be5c08c775f4a077a4ed9c128bf0041
Yashan97 <s.d.y.anupma@gmail.com>
2025-06-13 12:35:15 +0530

    Calculator Gui

--- DAY 02/Customer/src/Calculator.java
+++ DAY 02/Customer/src/Calculator.java
@@ -2,20 +2,121 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Calculator  extends JFrame {
-    JButton exitButton;
+    JButton btn1;
+    JButton btn2;
+    JButton btn3;
+    JButton btn4;
+    JButton btn5;
+    JButton btn6;
+    JButton btn7;
+    JButton btn8;
+    JButton btn9;
+    JButton btn10;
+    JButton btn11;
+    JButton btn12;
+    JButton btn13;
+    JButton btn14;
+    JButton btn15;
+    JButton btn16;
     JButton cancelBtn;
     JTextField name;
-    Calculator(){
+    Calculator() throws FontFormatException {
 
-        setLayout( new FlowLayout(FlowLayout.CENTER));
+        JPanel calTxt = new JPanel();
+        calTxt.setLayout(new GridLayout(1,1));
         name = new JTextField(10);
-        name.setText("name");
-        name.setFont(new Font("",10,15));
-        add(name);
+        name.setFont(new Font("",1,20));
+        calTxt.add(name);
+        add("North",calTxt);
 
-        setLayout((new FlowLayout(FlowLayout.CENTER)));
-        exitButton = new JButton();
-        exitButton.setText("Save");
-        add(exitButton);
+
+        JPanel calbutton = new JPanel();
+        calbutton.setLayout(new GridLayout(4,4));
+        btn1 = new JButton("7");
+        btn1.setFont(new Font(" ",1,10));
+        calbutton.add(btn1);
+
+        calbutton.setLayout(new GridLayout(4,4));
+        btn2 = new JButton("8");
+        btn2.setFont(new Font(" ",1,10));
+        calbutton.add(btn2);
+
+        calbutton.setLayout(new GridLayout(4,4));
+        btn3 = new JButton("9");
+        btn3.setFont(new Font(" ",1,10));
+        calbutton.add(btn3);
+
+        calbutton.setLayout(new GridLayout(4,4));
+        btn4 = new JButton("/");
+        btn4.setFont(new Font(" ",1,10));
+        calbutton.add(btn4);
+
+        calbutton.setLayout(new GridLayout(4,4));
+        btn5 = new JButton("4");
+        btn5.setFont(new Font(" ",1,10));
+        calbutton.add(btn5);
+
+        calbutton.setLayout(new GridLayout(4,4));
+        btn6 = new JButton("5");
+        btn6.setFont(new Font(" ",1,10));
+        calbutton.add(btn6);
+
+        calbutton.setLayout(new GridLayout(4,4));
+        btn7 = new JButton("6");
+        btn7.setFont(new Font(" ",1,10));
+        calbutton.add(btn7);
+
+        calbutton.setLayout(new GridLayout(4,4));
+        btn8 = new JButton("*");
+        btn8.setFont(new Font(" ",1,10));
+        calbutton.add(btn8);
+
+        calbutton.setLayout(new GridLayout(4,4));
+        btn9 = new JButton("1");
+        btn9.setFont(new Font(" ",1,10));
+        calbutton.add(btn9);
+
+        calbutton.setLayout(new GridLayout(4,4));
+        btn10 = new JButton("2");
+        btn10.setFont(new Font(" ",1,10));
+        calbutton.add(btn10);
+
+        calbutton.setLayout(new GridLayout(4,4));
+        btn11 = new JButton("3");
+        btn11.setFont(new Font(" ",1,10));
+        calbutton.add(btn11
+        );
+
+        calbutton.setLayout(new GridLayout(4,4));
+        btn12 = new JButton("-");
+        btn12.setFont(new Font(" ",1,10));
+        calbutton.add(btn12);
+
+        calbutton.setLayout(new GridLayout(4,4));
+        btn13 = new JButton("+");
+        btn13.setFont(new Font(" ",1,10));
+        calbutton.add(btn13);
+
+        calbutton.setLayout(new GridLayout(4,4));
+        btn14 = new JButton("=");
+        btn14.setFont(new Font(" ",1,10));
+        calbutton.add(btn14);
+
+        calbutton.setLayout(new GridLayout(4,4));
+        btn15 = new JButton(".");
+        btn15.setFont(new Font(" ",1,10));
+        calbutton.add(btn15);
+
+        calbutton.setLayout(new GridLayout(4,4));
+        btn16 = new JButton("0");
+        btn16.setFont(new Font(" ",1,10));
+        calbutton.add(btn16);
+
+        add("Center",calbutton);
+
+
+
+
+
     }
 }
--- DAY 02/Customer/src/Main.java
+++ DAY 02/Customer/src/Main.java
@@ -1,16 +1,15 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FontFormatException {
 
         Calculator  c1 = new Calculator();
-        c1.setSize(600, 600);
+        c1.setSize(300, 400);
         c1.setTitle("calculator");
         c1.setLocationRelativeTo(null);
         c1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         c1.setVisible(true);
-
-
 
 
     }
